7346d4cedc41b340468dd4d46560e7e97238342d
myrrhax <sizintsevdenis05@mail.ru>
2025-04-05 02:43:39 +0300

    Documentation

--- src/backend/Backend/InformationService/Program.cs
+++ src/backend/Backend/InformationService/Program.cs
@@ -1,13 +1,51 @@
+using Carter;
+using InformationService.Configuration;
+using InformationService.DataAccess;
+using InformationService.Entities;
+using InformationService.Interactors;
+using InformationService.Interactors.CreatePost;
+using InformationService.Interactors.DeletePost;
+using InformationService.Interactors.GetPost;
+using InformationService.Interactors.GetPosts;
+using InformationService.Interactors.UpdatePost;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthorization();
+builder.Services.AddDocumentation();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCarter();
+
+#region Interactors
+builder.Services.AddScoped<IBaseInteractor<CreatePostParams, Post>, CreatePostInteractor>();
+builder.Services.AddScoped<IBaseInteractor<DeletePostParams, Post>, DeletePostInteractor>();
+builder.Services.AddScoped<IBaseInteractor<Guid, Post>, GetPostInteractor>();
+builder.Services.AddScoped<IBaseInteractor<GetPostsParams, IEnumerable<Post>>, GetPostsInteractor>();
+builder.Services.AddScoped<IBaseInteractor<UpdatePostParams, Post>, UpdatePostInteractor>();
+#endregion
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(opt =>
+    {
+        opt.RouteTemplate = "openapi/{documentName}.json";
+    });
+
+    app.MapScalarApiReference(opt =>
+    {
+        opt.Title = "WebChatAPI";
+        opt.Theme = ScalarTheme.Mars;
+        opt.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+    });
 }
 
 app.UseHttpsRedirection();
