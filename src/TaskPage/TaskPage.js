f3992075fbd9e2c67368ce908c7757e6f1ad8b3e
Artem Moroz <moroz888a@gmail.com>
2025-04-05 15:40:05 +0300

    Tasks 0.2

--- src/TaskPage/TaskPage.js
+++ src/TaskPage/TaskPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./TaskPage.css";
-import TaskCard from "./TaskCard";
+import TaskCard from "../components/TaskCard/TaskCard";
 
 function TaskPage() {
   return (
