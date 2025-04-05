3b96717847dd7c59dd68871f3213a034d8619f68
Artem Moroz <moroz888a@gmail.com>
2025-04-06 02:09:47 +0300

    patch welcomepage

--- src/WelcomePage/WelcomePage.js
+++ src/WelcomePage/WelcomePage.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./WelcomePage.css";
-import AuthForm from "../components/AuthForm/AuthForm";
 
-function LoginPage() {
+function WelcomePage() {
   return (
-    <div className="loginWrap">
-      <AuthForm mode="login"/>
-    </div>
+    <main className="welcome-container">
+      <div className="welcome-text">
+        <h1>Добро пожаловать!</h1>
+        <h2>Офисный Веб-Органайзер</h2>
+      </div>
+      <div className="divider divider-firstr"></div>
+      <div className="divider divider-second"></div>
+      <div className="button-group">
+        <Link to="/login" className="button">
+          Войти
+        </Link>
+        <Link to="/register" className="button">
+          Зарегистрироваться
+        </Link>
+      </div>
+    </main>
   );
 }
 
-export default LoginPage;
+export default WelcomePage;
