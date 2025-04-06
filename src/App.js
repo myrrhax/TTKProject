36e5f4bb21090a68a482651443e95153bf13aab6
Artem Moroz <moroz888a@gmail.com>
2025-04-06 04:58:44 +0300

    routers patch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-
+import WelcomePage from "./WelcomePage/WelcomePage";
 import LoginPage from "./LoginPage/LoginPage";
 import RegisterPage from "./RegisterPage/RegisterPage";
 import TaskPage from "./TaskPage/TaskPage";
 import AdminPage from "./AdminPage/AdminPage";
-import ReaderPage from "./ReaderPage/Reader";
 import ArticlesPage from "./ArticlesPage/ArticlesPage";
+import ReaderPage from "./ReaderPage/Reader";
 import WriterPage from "./WriterPage/WriterPage";
-import WelcomePage from "./WelcomePage/WelcomePage";
 import "./App.css";
 
 function App() {
